1f69f5448c0b869d8f37633b3ee9e2e2594c4ccd
shbrief <shbrief@hotmail.com>
2016-08-23 21:31:23 -0400

    Create Frequent Words.py

---
+++ Frequent Words.py
@@ -0,0 +1,16 @@
+import sys
+lines = sys.stdin.read().splitlines() # read in the input from STDIN
+Text = lines[0]
+k = int(lines[1])
+
+kmer_dict = dict()
+
+for i in range(len(Text)-k+1):
+	if Text[i:i+k] in kmer_dict:
+		kmer_dict[Text[i:i+k]] += 1
+	else:
+		kmer_dict[Text[i:i+k]] = 1
+
+kmers = [item[0] for item in kmer_dict.items() if item[1] == max(kmer_dict.values())]
+
+print(" ".join(kmers))

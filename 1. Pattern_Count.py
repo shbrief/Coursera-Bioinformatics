62e0a84d4d422bcdde376b0ba7fec46f5ed935bd
shbrief <shbrief@hotmail.com>
2016-08-24 16:09:36 -0400

    Update 1. Pattern_Count.py

--- 1. Pattern_Count.py
+++ 1. Pattern_Count.py
@@ -3,11 +3,11 @@ lines = sys.stdin.read().splitlines() # read in the input from STDIN
 Text = lines[0]
 Pattern = lines[1]
 
-def PatternCount(Text, Pattern):
+def pattern_count(Text, Pattern):
     count = 0
     for i in range(len(Text)-len(Pattern)+1):
         if Text[i:i+len(Pattern)] == Pattern:
             count = count+1
     return count
 
-print(PatternCount(Text,Pattern))
+print(pattern_count(Text,Pattern))

5ce995eb7e58f4bad13595a6de530979c81b6442
shbrief <shbrief@hotmail.com>
2016-08-24 16:04:58 -0400

    Update 2. Frequent_Words.py

--- 2. Frequent_Words.py
+++ 2. Frequent_Words.py
@@ -6,11 +6,11 @@ k = int(lines[1])
 def FrequentWords(Text, k):    
     kmer_dict = dict()
     for i in range(len(Text)-k+1):
-	    if Text[i:i+k] in kmer_dict:
-		    kmer_dict[Text[i:i+k]] += 1
-	    else:
-		    kmer_dict[Text[i:i+k]] = 1
+	if Text[i:i+k] in kmer_dict:
+            kmer_dict[Text[i:i+k]] += 1
+	else:
+	    kmer_dict[Text[i:i+k]] = 1
     kmers = [item[0] for item in kmer_dict.items() if item[1] == max(kmer_dict.values())]
-    return (' '.join(kmers))
+    return(' '.join(kmers))
 
 print(FrequentWords(Text,k))

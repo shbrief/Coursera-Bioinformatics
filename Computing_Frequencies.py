28152ffcffa02ac93f60fc4ed757d0abd5db7f0f
shbrief <shbrief@hotmail.com>
2016-08-27 19:54:26 -0400

    Create Computing_Frequencies.py
    compute the frequency array of the given sequence

---
+++ Computing_Frequencies.py
@@ -0,0 +1,17 @@
+nucleotide = {"A":0, "C":1, "G":2, "T":3}
+def pattern_to_number(Pattern):
+    if len(Pattern) == 0:
+        return 0    
+    symbol = Pattern[-1]
+    prefix = Pattern[0:-1]
+    return (4 * pattern_to_number(prefix) + nucleotide.get(symbol))
+        
+def computing_frequencies(Text, k):
+    frequency_array = [None]*(4**k)
+    for i in range(4**k):
+        frequency_array[i] = 0
+    for i in range(len(Text)-k+1):
+        pattern = Text[i:i+k]
+        j = pattern_to_number(pattern)
+        frequency_array[j] = frequency_array[j] + 1
+    return (" ".join(str(e) for e in frequency_array))
